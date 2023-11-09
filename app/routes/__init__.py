80540f31236eca96fc73b1769bd00cf0851f89b6
qbandres <qbandres@gmail.com>
2023-11-09 14:10:40 -0500

    Se optimizo el codigo de app/routes/__init__.py para iuna mejor lectura

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,13 @@
-from .main import *
-from .index import *
-from .agregar_montaje import *
-from .anadir_elemento import *
-from .dashboardFilter import *
+# __init__.py dentro de la carpeta routes
+
+# Importaciones específicas de cada archivo de ruta. Esto asegura que solo se importen las funciones necesarias.
+# Además, mejora la claridad del código y facilita el mantenimiento y la escalabilidad de la aplicación.
+
+from .main import generate_graph_data, main, download_excel
+from .index import index, login
+from .agregar_montaje import agregar_montaje, actualizar_montaje, handle_procesar_busqueda_actualizar
+from .anadir_elemento import anadir_elemento
+from .dashboardFilter import get_select_data, graphic_dash
+
+# Cada función se importa explícitamente por su nombre, evitando así conflictos de nombres y 
+# la importación de funciones no utilizadas. Asegúrate de que estas funciones se usen en el resto de la aplicación.
